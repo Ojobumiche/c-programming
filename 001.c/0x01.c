787dfbecdda78e379f6261e57690c830ab9aa4ed
Ojobumiche <bumiche@gmail.com>
2023-01-18 21:19:36 -0800

    practice makes perfect

---
+++ 001.c/0x01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/***
+ * main - the entry point of the program
+ *
+ * printf: to display the alphabet of letters
+ *
+ * Return: Always 0.
+ **/
+
+
+int main()
+
+{
+	printf("alphabet in lower case:\n
+			a\n b\n c\n d\n f\n g\n h\n i\n j\n k\n l\n m\n n\n o\n p\n r\n s\n t\n u\n v\n w\n x\n y\n z");
+
+
+	return(0);
+
+}
