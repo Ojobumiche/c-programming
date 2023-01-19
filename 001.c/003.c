462ef7607f76d95291d5320febbdc81867961cf3
Ojobumiche <bumiche@gmail.com>
2023-01-18 22:05:14 -0800

    ok

---
+++ 001.c/003.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+/***
+ *
+ * main - the entry point of the program
+ *
+ * @user : userInput
+ *
+ * return; Always 0;
+ *
+ */
+
+int main()
+{
+	int num;
+
+
+	printf("Enter two numbers");
+
+
+	scanf("%d". &num);
+
+	if(num== odd)
+	{
+		printf("%d userInput is odd\n", num);
+	}
+	else if(num== even)
+	{
+		printf("%d userInput is even\n", num);
+	}
+	else{
+		printf("this is zero %d\n", num);
+	}
+
+
+	return(0);
+
+}
